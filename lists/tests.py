e4adfeebd249922b8cef3fb3305d1b6c35f95fc4
Vsevolod Svirin <vsevolodsvirin@Ilya-Mihailenko.local>
2017-01-31 17:47:52 +0300

    Model for list Items and associated migration

--- lists/tests.py
+++ lists/tests.py
@@ -2,11 +2,25 @@ from django.core.urlresolvers import resolve
 from django.test import TestCase
 from django.http import HttpRequest
 from django.template.loader import render_to_string
+from django.shortcuts import render
+
+import re
 
 from lists.views import home_page
+from lists.models import Item
 
 
 class HomePageTest(TestCase):
+    @staticmethod
+    def remove_csrf(html_code):
+        csrf_regex = r'<input[^>]+csrfmiddlewaretoken[^>]+>'
+        return re.sub(csrf_regex, '', html_code)
+
+    def assertEqualExceptCSRF(self, html_code1, html_code2):
+        return self.assertEqual(
+            self.remove_csrf(html_code1),
+            self.remove_csrf(html_code2)
+        )
 
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
@@ -15,5 +29,41 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
-        expected_html = render_to_string('home.html')
-        self.assertEqual(response.content.decode(), expected_html)
+        # expected_response = render(request, 'home.html')
+        # self.assertEqual(response.content.decode(), expected_response.content.decode())
+        expected_html = render_to_string('home.html', request=request)
+        self.assertEqualExceptCSRF(response.content.decode(), expected_html)
+
+    def test_home_page_can_save_a_POST_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+
+        response = home_page(request)
+
+        expected_response = render(
+            request,
+            'home.html',
+            {'new_item_text': 'A new list item'}
+        )
+        self.assertEqualExceptCSRF(response.content.decode(), expected_response.content.decode())
+
+
+class ItemModelTest(TestCase):
+
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(second_saved_item.text, 'Item the second')
